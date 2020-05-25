843fc790e56675fa041418f779efaad6e5b25092
Marco Biedermann <marco.biedermann@gmx.de>
2020-05-25 09:23:29 +0200

    fix: create pages
    Fix create pages hook in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,16 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
     query {
+      allShopifyCollection(sort: { fields: [title] }) {
+        edges {
+          node {
+            title
+            shopifyId
+            handle
+            description
+          }
+        }
+      }
       allShopifyProduct(sort: { fields: [title] }) {
         edges {
           node {
@@ -29,40 +39,22 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  result.data.allShopifyProduct.edges.forEach(({ node }) => {
-    createPage({
-      path: `/products/${node.handle}`,
-      component: path.resolve(`./src/templates/product.tsx`),
-      context: {
-        product: node,
-      },
-    })
-  })
-}
-
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allShopifyCollection(sort: { fields: [title] }) {
-        edges {
-          node {
-            title
-            shopifyId
-            handle
-            description
-          }
-        }
-      }
-    }
-  `)
-
   result.data.allShopifyCollection.edges.forEach(({ node }) => {
     createPage({
       path: `/collections/${node.handle}`,
       component: path.resolve(`./src/templates/collection.tsx`),
       context: {
         collection: node,
+      },
+    })
+  })
+
+  result.data.allShopifyProduct.edges.forEach(({ node }) => {
+    createPage({
+      path: `/products/${node.handle}`,
+      component: path.resolve(`./src/templates/product.tsx`),
+      context: {
+        product: node,
       },
     })
   })
