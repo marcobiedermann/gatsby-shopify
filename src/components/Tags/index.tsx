7d695c329ccd280335b96fee4ce97c6b9a1f2637
Marco Biedermann <marco.biedermann@gmx.de>
2020-11-25 13:46:08 +0100

    feat: Tags
    Style tags

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import Tag from '../Tag';
+import styles from './style.module.css';
 
 export interface TagsProps {
   tags: string[];
@@ -9,7 +10,7 @@ const Tags: FC<TagsProps> = (props) => {
   const { tags } = props;
 
   return (
-    <ul>
+    <ul className={styles.tags}>
       {tags.map((tag) => (
         <li key={tag}>
           <Tag>{tag}</Tag>
